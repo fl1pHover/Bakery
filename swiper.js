6d89f87d40694fe28171ee31ed79173a1740c8e9
fl1pHover <93395269+fl1pHover@users.noreply.github.com>
2022-04-14 10:33:28 +0800

    Add files via upload

--- swiper.js
+++ swiper.js
@@ -1,34 +1,37 @@
 var swiper = new Swiper('.swiper', {
     // Optional parameters
+
     direction: 'horizontal',
     spaceBetween: 30,
     loop: false,
-    
-    
+
+
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
     },
-    
+
     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    
+
     // And if we need scrollbar
     scrollbar: {
         el: '.swiper-scrollbar',
     },
     effect: 'coverflow',
-    grabCursor: true, 
+    grabCursor: true,
     centeredSlides: true,
     slidesPerView: 'auto',
     coverflowEffect: {
         rotate: 0,
         depth: 100,
-        stretch:0,
-        slideShadows: false,
-      modifier: 2,
+        stretch: 0,
+        slideShadows: true,
+        modifier: 2,
     },
+
+
 });
